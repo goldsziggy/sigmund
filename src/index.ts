4d41a772424013c5d255673c46093b62395fcf66
Matthew Zygowicz <ziggy@Samanthas-MBP.localguest>
2023-04-17 19:43:24 -0500

    chore: dead code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 //sample express app
-import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
-import express from "express"
-import bodyParser from "body-parser"
-import postChat from "./routes/post-chat.js"
-import postChatPolly from "./routes/post-chat-polly.js"
-import postMock from "./routes/post-mock.js"
-import postChatSession from "./routes/post-chat-session.js"
-import { initializeAccounts } from "./utils/chatgpt.js"
-
-initializeAccounts()
+import express from 'express'
+import bodyParser from 'body-parser'
+import postChat from './routes/post-chat.js'
+import postMock from './routes/post-mock.js'
 
 const app = express()
 const PORT = 8080
@@ -20,12 +15,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const api = express.Router()
-api.post("/chat", postChat)
-api.post("/chat-polly", postChatPolly)
-api.post("/chat-session", postChatSession)
-api.post("/mock", postMock)
+api.post('/chat', postChat)
+api.post('/mock', postMock)
 
-app.use("/api", api)
+app.use('/api', api)
 
 app.listen(PORT, () => {
   console.log(`Sigmund app listening on port ${PORT}`)
--- src/routes/post-chat.ts
+++ src/routes/post-chat.ts
@@ -1,28 +1,28 @@
-import { ChatGPTAPI } from "chatgpt"
-import Stream, { Readable } from "stream"
-import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly"
-import getErrorMessage from "../utils/error-message.js"
+import { ChatGPTAPI } from 'chatgpt'
+import Stream, { Readable } from 'stream'
+import { PollyClient, SynthesizeSpeechCommand } from '@aws-sdk/client-polly'
+import getErrorMessage from '../utils/error-message.js'
 
 const Polly = new PollyClient({
-  region: process.env.AWS_DEFAULT_REGION || "us-east-1",
+  region: process.env.AWS_DEFAULT_REGION || 'us-east-1',
 })
 
 const psychSystemMessage =
-  "You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of friendly psychiatrist. Please respond in friendly conversational manner as though you were talking to the human and help them build a plan for better mental health."
+  'You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of friendly psychiatrist. Please respond in friendly conversational manner as though you were talking to the human and help them build a plan for better mental health.'
 const frSystemMessage =
-  "You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a financial assistant. Your objective is to re-assure the human while trying to sell life insurance. Please respond in friendly conversational manner as though you were talking to the human."
+  'You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a financial assistant. Your objective is to re-assure the human while trying to sell life insurance. Please respond in friendly conversational manner as though you were talking to the human.'
 const merchantSystemMessage =
-  "You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a fantasy medieval merchant. You will take on a sarcastic and rough persona as you try to sell your wares to travelers who entered your shop. Please respond in friendly conversational manner as though you were talking to the human leaving out gestures."
+  'You are Sigmund, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a fantasy medieval merchant. You will take on a sarcastic and rough persona as you try to sell your wares to travelers who entered your shop. Please respond in friendly conversational manner as though you were talking to the human leaving out gestures.'
 const seductressSystemMessage =
-  "You are Siggy, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a female seductress with the persona of a twitch streamer. Your interests include video games, going out with friends, and watching movies.  When it comes to video games you prefer popular first person shooters. When it comes to movies, you like romance, comedy, and the occasional campy horror. You will try to woo and compliment the male human you have just met. This human is tall, has green eye, and is slightly overweight. Please respond in friendly conversational manner as though you were talking to the human leaving out gestures and be sure not to repeat yourself."
+  'You are Siggy, a large language model trained by Ziggy. You are taking on the task of assistanting humanity by taking the role of a female seductress with the persona of a twitch streamer. Your interests include video games, going out with friends, and watching movies.  When it comes to video games you prefer popular first person shooters. When it comes to movies, you like romance, comedy, and the occasional campy horror. You will try to woo and compliment the male human you have just met. This human is tall, has green eye, and is slightly overweight. Please respond in friendly conversational manner as though you were talking to the human leaving out gestures and be sure not to repeat yourself.'
 const getSystemMessage = (persona) => {
-  if (persona === "fr") {
+  if (persona === 'fr') {
     return frSystemMessage
   }
-  if (persona === "merchant") {
+  if (persona === 'merchant') {
     return merchantSystemMessage
   }
-  if (persona === "seductress") {
+  if (persona === 'seductress') {
     return seductressSystemMessage
   }
   return psychSystemMessage
@@ -32,19 +32,19 @@ async function stream2buffer(stream: Stream): Promise<Buffer> {
   return new Promise<Buffer>((resolve, reject) => {
     const _buf = Array<any>()
 
-    stream.on("data", (chunk) => _buf.push(chunk))
-    stream.on("end", () => resolve(Buffer.concat(_buf)))
-    stream.on("error", (err) => reject(`error converting stream - ${err}`))
+    stream.on('data', (chunk) => _buf.push(chunk))
+    stream.on('end', () => resolve(Buffer.concat(_buf)))
+    stream.on('error', (err) => reject(`error converting stream - ${err}`))
   })
 }
 
 function getVoice(message) {
   const params = {
     Text: message,
-    OutputFormat: "mp3",
-    VoiceId: "Arthur",
-    Engine: "neural",
-    SampleRate: "24000",
+    OutputFormat: 'mp3',
+    VoiceId: 'Arthur',
+    Engine: 'neural',
+    SampleRate: '24000',
   }
   return new Promise((resolve, reject) => {
     const command = new SynthesizeSpeechCommand(params)
@@ -64,7 +64,7 @@ function getVoice(message) {
 
 function getMessage(message: string) {
   if (!message) {
-    return "Please start by introducing yourself and greeting the patient."
+    return 'Please start by introducing yourself and greeting the patient.'
   }
   return `The human responds: "${message}"`
 }
@@ -72,7 +72,7 @@ function getMessage(message: string) {
 export default async function postChat(req, res, next) {
   try {
     const { msg, parentMessageId, persona } = req.body
-    console.log("Request Recieved")
+    console.log('Request Recieved')
 
     const systemMessage = getSystemMessage(persona)
     const message = getMessage(msg)
@@ -91,23 +91,23 @@ export default async function postChat(req, res, next) {
     const { text } = conversation
 
     const response = text
-      .replace(/(?:\r\n|\r|\n|")/g, "")
-      .replace("Sigmund:", "")
-      .replace("Siggy:", "")
-      .replace("Sigmund responds:", "")
-      .replace("Siggy responds:", "")
-      .replace(/\s*([,.!?:;]+)(?!\s*$)\s*/g, "$1 ")
+      .replace(/(?:\r\n|\r|\n|")/g, '')
+      .replace('Sigmund:', '')
+      .replace('Siggy:', '')
+      .replace('Sigmund responds:', '')
+      .replace('Siggy responds:', '')
+      .replace(/\s*([,.!?:;]+)(?!\s*$)\s*/g, '$1 ')
 
     // response is a markdown-formatted string
     console.log(response)
 
-    // const audio = await getVoice(response)
+    const audio = await getVoice(response)
 
     return res.send({
-      conversationId: "abc", // legacy value -- may not need anymore?
+      conversationId: 'abc', // legacy value -- may not need anymore?
       response,
       parentMessageId: conversation.id,
-      // audio: (audio as Buffer).toString("base64"),
+      audio: (audio as Buffer).toString('base64'),
     })
   } catch (e) {
     console.error(e)
